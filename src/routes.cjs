c1595816600f566b7eb1578b3efa6c8905f4f4bb
Connor Clark <connordeclark@gmail.com>
2023-07-05 15:17:06 -0500

    Test

--- src/routes.cjs
+++ src/routes.cjs
@@ -11,10 +11,10 @@ const { chromium } = require('playwright');
   await page.waitForTimeout(1000); // Add a delay of 1 second before interacting with the elements
   await page.getByLabel('Email or phone').click();
   await page.getByLabel('Email or phone').fill('connordeclark@gmail.com');
-  await page.getByLabel('Password', { exact: true }).fill('!K0di@k92)');
+  await page.getByLabel('Password', { exact: true }).fill('');
   await page.getByRole('button', { name: 'Sign in', exact: true }).click();
   await page.getByRole('link', { name: 'Jobs' }).click();
-  await page.waitForLoadState('networkidle');
+  await page.waitForLoadState('networkidle')
   await page.waitForSelector('#recentSearchesIndex__0');
   await page.locator('#recentSearchesIndex__0').click();
   const jobs = await page.$eval('.scaffold-layout__list-container', el => {
